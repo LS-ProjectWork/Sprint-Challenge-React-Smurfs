6858c7e0c6445a94f757db0c569cff17f5460cac
dpayton23 <darien.payton71@gmail.com>
2019-04-05 15:37:52 -0400

    created addItem to add new smurf to DB. Added navlinks and route for components

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Route, NavLink} from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -12,7 +13,7 @@ class App extends Component {
       smurfs: [],
     };
   }
-  
+
   componentDidMount() {
     axios.get('http://localhost:3333/smurfs')
     .then(res => {
@@ -23,14 +24,29 @@ class App extends Component {
       console.log(err)
     })
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  addItem = newItem => {
+    axios.post('http://localhost:3333/smurfs', newItem)
+    .then(res => {
+      this.setState({smurfs: res.data})
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <NavLink to='/'>Smurfs</NavLink>
+          <NavLink to='/smurf-form'>Add Smurf</NavLink>
+        </nav>
+        <Route exact path='/' render={props => <Smurfs {...props} 
+        smurfs={this.state.smurfs} />} />
+        <Route path='/smurf-form' render={props => <SmurfForm {...props} 
+        addItem={this.addItem} />} />
       </div>
     );
   }
